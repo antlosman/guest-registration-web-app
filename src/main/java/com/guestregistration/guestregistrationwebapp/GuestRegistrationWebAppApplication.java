30ed649dc38470d88e9f7b51e772cc785a80c83b
Anton Losman <87125898+antlosman@users.noreply.github.com>
2022-09-03 16:35:35 +0300

    [init] - project skeleton created

---
+++ src/main/java/com/guestregistration/guestregistrationwebapp/GuestRegistrationWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.guestregistration.guestregistrationwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuestRegistrationWebAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GuestRegistrationWebAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/guestregistration/guestregistrationwebapp/GuestRegistrationWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guestregistration.guestregistrationwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuestRegistrationWebAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
