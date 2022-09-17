a18d2969d3efa28f83c85fe32d3cf081ac96b717
Anton Losman <87125898+antlosman@users.noreply.github.com>
2022-09-17 19:09:22 +0300

    [add] - Event entity added

--- backend/src/main/java/com/guestregistration/guestregistrationwebapp/GuestRegistrationWebAppApplication.java
+++ backend/src/main/java/com/guestregistration/guestregistrationwebapp/GuestRegistrationWebAppApplication.java
@@ -2,7 +2,9 @@ package com.guestregistration.guestregistrationwebapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
+@EntityScan("<entity>")
 @SpringBootApplication
 public class GuestRegistrationWebAppApplication {
 
---
+++ backend/src/main/java/com/guestregistration/guestregistrationwebapp/entity/Event.java
@@ -0,0 +1,35 @@
+package com.guestregistration.guestregistrationwebapp.entity;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import java.time.LocalDate;
+
+import static javax.persistence.GenerationType.SEQUENCE;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class Event {
+
+    @Id
+    @SequenceGenerator(name = "event_sequence", sequenceName = "event_sequence", allocationSize = 1)
+    @GeneratedValue(strategy = SEQUENCE, generator = "event_sequence")
+    private Long id;
+
+    private String name;
+
+    private LocalDate date;
+
+    private int participantsQuantity;
+
+}
