73c98db5aa8535dee2ba936dbaa418db18562d2f
Anton Losman <87125898+antlosman@users.noreply.github.com>
2022-09-21 15:10:28 +0300

    [add] - past-events component added to frontend + some changes in css code

--- frontend/src/app/components/past-events/past-events.component.ts
+++ frontend/src/app/components/past-events/past-events.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {FutureEventsService} from "../../services/future-events.service";
+import {Event} from "../../models/event";
+
 
 @Component({
   selector: 'app-past-events',
@@ -7,9 +10,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PastEventsComponent implements OnInit {
 
-  constructor() { }
+  futureEvents: Array<Event> = []
+
+  constructor(private futureEventsService: FutureEventsService) { }
 
   ngOnInit(): void {
+    this.futureEventsService
+      .getFutureEvents()
+      .subscribe(value => {this.futureEvents = value});
   }
+
+  displayedColumns: string[] = ['id', 'name', 'date', 'p-quantity'];
+  groupedColumns: string[] = ['grouped'];
 
 }
